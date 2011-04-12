000aa552ca79157f20af370a18f617e62cf617e0
Lars Haugan <larhauga@ebnett.no>
2011-04-12 03:43:23 +0200

    Laget innloggingsside for adminpanelet

---
+++ produkt/admin/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Innlogging - Admin</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<?php
+if(true)
+{
+	echo '
+		<div id="containerLogin">
+			<div id="menyline">
+				<p><a href="../index.php">Tilbake</a></p>
+			</div>
+			<div id="loginboks">
+				<h1>Login</h1>
+				<p>Uavtorisert tilgang vil bli logget!</p>
+				<form id="adminlogin">
+				<p>Brukernavn: <input type="text" id="user" name="bruker" /></p>
+				<p>Passord: <input type="password" id="psw" name="passord" /></p>
+				<p class="forgetmenot">
+					<label><input name="rememberme" type="checkbox" id="rememberme" value="forever" tabindex="90" /> Husk meg</label>
+				</p>
+				<p><input type="submit" value="Logg inn" /></p>
+				</form>
+			</div>
+		</div>
+		';
+}
+else
+{
+	echo '
+	<div id="container">
+		<div id="head">
+			
+		</div>
+		<div id="meny">
+			
+		</div>
+		<div id="content">
+			
+		</div>
+		<div id="footer">
+			
+		</div>
+	</div>
+	';
+}
+?>
+</body>
+</html>
