465ffcfedac9f7c8b37087a8f785a4a29ff1d502
Lars Haugan <larhauga@ebnett.no>
2011-04-12 23:39:14 +0200

    Konfigurering av adminpanelet

--- produkt/admin/index.php
+++ produkt/admin/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <?php
-if(true)
+if(false) //Not logged inn
 {
 	echo '
 		<div id="containerLogin">
@@ -48,13 +48,42 @@ else
 	</div>
 	<div id="container">
 		<div id="head">
-			
+			<h1>Admin - Hjem</h1>
 		</div>
 		<div id="meny">
-			
+			<ul>
+				<li><a href="#">Hjem</a></li>
+				<li><a href="#">Brukere</a></li>
+				<li><a href="#">Kategorier</a></li>
+				<li><a href="#">Produkter</a></li>
+				<li><a href="#">Varebeholdning</a></li>
+				<li><a href="#">Sikkerhet</a></li>
+				<li><a href="#">Konfigurering</a></li>
+			</ul>
 		</div>
 		<div id="content">
-			
+			<table width="100%">
+				<tr>
+					<td align="center"><img src="bilder/user-icon.png" alt="Brukere" width="150" height="150"/></td>
+					<td align="center"><img src="bilder/folders.jpg" alt="Kategorier" width="150" height="150"/></td>
+					<td align="center"><img src="bilder/Cardboard-Box.png" alt="Produkter" width="150" height="150"/></td>
+				</tr>
+				<tr>
+					<td align="center">Brukere</td>
+					<td align="center">Kategorier</td>
+					<td align="center">Produkter</td>
+				</tr>
+				<tr>
+					<td align="center"><img src="bilder/barcode.jpg" alt="Varebeholdning" width="150" height="150"/></td>
+					<td align="center"><img src="bilder/windows-7-security-icon.png" alt="Sikkerhet" width="150" height="150"/></td>
+					<td align="center"><img src="bilder/Config-Tools.png" alt="Konfigurering" width="150" height="150"/></td>
+				</tr>
+				<tr>
+					<td align="center">Varebeholdning</td>
+					<td align="center">Sikkerhet</td>
+					<td align="center">Konfigurering</td>
+				</tr>
+			</table>
 		</div>
 		<div id="footer">
 			
