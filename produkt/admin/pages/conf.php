85402b683dc2acda82d6c8117b5e4cb96fedae7a
Lars Haugan <larhauga@ebnett.no>
2011-04-13 14:54:01 +0200

    Konfigurert adminsiden for aa passe alle nettlesere ink. internet explorer

--- produkt/admin/pages/conf.php
+++ produkt/admin/pages/conf.php
@@ -1,10 +1,13 @@
 <?php
 	echo '
-			<h1><img src="images/Config-Tools.png" alt="Brukere" width="30" height="30" />Sikkerhet</h1>
-			
+			<h1><img src="images/Config-Tools.png" alt="Brukere" width="30" height="30" />Instillinger</h1>
+			Tittel, slagord, adresse (url), epost, rolle for nye brukere,<br/>
+			datoformat, tidsformat.<br/>
+				-> Media: Miniatyrbildestørelse, medium, stor størelse av bilder</br>
+					velge path for filopplastning<br/>
+					administrere opplastede filer<br/>
 			- Konfigurere nettstedet<br/>
 			- Tittel, datoformat, plass, epost.<br/>
 			- Brukerinstillinger
-			
 ';
 ?>
