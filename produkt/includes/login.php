066071bee40d86c6754ddb22eab58c7b0919ba78
Flanders <anders.struksnes@gmail.com>
2011-04-29 20:05:21 +0200

    login fiksing...

--- produkt/includes/login.php
+++ produkt/includes/login.php
@@ -30,7 +30,7 @@ if(!isset($_GET['login']))
                 $bruker->login($passord,$epost);
              }
             
-            if($login)
+            if(login)
             {
                  echo 'hei';
                 
