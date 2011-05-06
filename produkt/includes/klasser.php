564dd42252023da9377ae7778c71372c0c5edf7f
Ole Henrik <Ole@xzindor.com>
2011-05-06 21:09:12 +0200

    fikser enda på handlevogna

--- produkt/includes/klasser.php
+++ produkt/includes/klasser.php
@@ -114,7 +114,42 @@ class handlekurv
 		}
 	function aktivHandlekurv()
 	{
+	$cart = $_SESSION['cart'];
+	if ($cart) 
+	{
+		$items = explode(',',$cart);
+		$contents = array();
+		foreach ($items as $item) 
+		{
+		$contents[$item] = (isset($contents[$item])) ? $contents[$item] + 1 : 1;
+		}
+	$output[] = '<form action="cart.php?action=update" method="post" id="cart">';
+	$output[] = '<table>';
+	foreach ($contents as $id=>$qty) {
+	$sql = 'SELECT * FROM varer WHERE id = '.$id;
+	$result = $db->query($sql);
+	$row = $result->fetch();
+	extract($row);
+	$output[] = '<tr>';
+	$output[] = '<td><a href="cart.php?action=delete&id='.$id.'" class="r">Slett</a></td>';
+	$output[] = '<td>'.$title.' by '.$author.'</td>';
+	$output[] = '<td>&pound;'.$price.'</td>';
+	$output[] = '<td><input type="text" name="qty'.$id.'" value="'.$qty.'" size="3" maxlength="3" /></td>';
+	$output[] = '<td>&pound;'.($price * $qty).'</td>';
+	$total += $price * $qty;
+	$output[] = '</tr>';
 	}
+	
+		$output[] = '</table>';
+		$output[] = '<pTotalsum:'.$total.'</p>';
+		$output[] = '<div><button type="submit">Opptater handlevogn</button></div>';
+		$output[] = '</form>';
+	} else 
+	{
+		$output[] = '<p>Handlevogna er tom..</p>';
+	}
+return join('',$output);
+}
 	}
 
 
