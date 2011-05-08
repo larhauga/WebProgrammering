27e1a26e9e1c29b3e5c7921dde84f2903e660d80
Ole Henrik <Ole@xzindor.com>
2011-05-09 00:12:16 +0200

    ordrelinje tull

---
+++ produkt/includes/handlekurv.php
@@ -0,0 +1,76 @@
+<?php
+
+
+/*
+if(!session_started())
+{
+session_start();
+}*/
+
+include_once("class_handlekurv.php");
+$db = mysql_connect("193.107.29.49", "xzindor_db1", "lol123") or die(mysql_error());
+$handlekurv = $_SESSION['handlekurv'];
+//$action = $_GET['action'];
+if(isset($_GET['action']))
+{
+    $action = $_GET['action'];
+switch ($action) {
+	case 'add':
+		if ($handlekurv) {
+			$handlekurv .= ','.$_GET['id'];
+		} else {
+			$handlekurv = $_GET['id'];
+		}
+		break;
+	case 'delete':
+		if ($handlekurv) {
+			$varer = explode(',',$handlekurv);
+			$newcart = '';
+			foreach ($varer as $vare) {
+				if ($_GET['id'] != $vare) {
+					if ($newcart != '') {
+						$newcart .= ','.$vare;
+					} else {
+						$newcart = $vare;
+					}
+				}
+			}
+			$handlekurv = $newcart;
+		}
+		break;
+	case 'update':
+	if ($handlekurv) {
+		$newcart = '';
+		foreach ($_POST as $key=>$value) {
+			if (stristr($key,'antall')) {
+				$id = str_replace('antall','',$key);
+				$varer = ($newcart != '') ? explode(',',$newcart) : explode(',',$handlekurv);
+				$newcart = '';
+				foreach ($varer as $vare) {
+					if ($id != $vare) {
+						if ($newcart != '') {
+							$newcart .= ','.$vare;
+						} else {
+							$newcart = $vare;
+						}
+					}
+				}
+				for ($i=1;$i<=$value;$i++) {
+					if ($newcart != '') {
+						$newcart .= ','.$id;
+					} else {
+						$newcart = $id;
+					}
+				}
+			}
+		}
+	}
+	$handlekurv = $newcart;
+	break;
+}
+}
+$_SESSION['handlekurv'] = $handlekurv;
+$fin= 2;
+$kurv = new handlekurv($fin);
+echo $kurv->visHandlekurv();
+?>
