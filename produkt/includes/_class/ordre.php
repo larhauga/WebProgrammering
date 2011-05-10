e1ef6a221e074fc8402d45f3d3870a76f91dbd4d
Ole Henrik <Ole@xzindor.com>
2011-05-10 15:16:59 +0200

    fikse en bugg av 10000

--- produkt/includes/_class/ordre.php
+++ produkt/includes/_class/ordre.php
@@ -1,26 +1,31 @@
 <?php
 
-class ordre
+class ordre extends dbase
 {
 	public $kundeid;
 	public $dato;
 	public $betalt;
 	public $total;
 	
-	function __construct()
-	{
-	}
-	function addOrdreLinje($innAntall,$innID)
+	    public function __construct()
+    	{
+        parent::__construct();
+    	}
+		
+		
+	function addOrdreLinje($innAntall,$innID,$ordreid)
 	{
 		//komando for å skrive dette til sqlen
-		$mysqli = new mysqli('193.107.29.49','xzindor_db1','lol123','xzindor_db1');
+		$mysqli = parent::connect();
 		$sql2 = "SELECT pris FROM vare WHERE idvare = $id;";
 		$resultat = $mysqli->query($sql2); 
 		// mysql sjekk her :)
-		$pris = $resultat;
+		$arraytt = mysqli_fetch_row($resultat);
+		$pris = $valg[6];
 		$resultat = "";
-		$sql = "Insert into ordrelinje(idvare,prisPrEnhet,antall) 
+		$sql = "Insert into ordrelinje(idordre,idvare,prisPrEnhet,antall) 
 		Values(
+		'$ordreid',
 		'$innID',
 		'$pris',
 		'$innAntall')";
@@ -37,23 +42,34 @@ class ordre
                           
            $bruker = unserialize($_SESSION['bruker']);
            $epost=$bruker->epost;
-		  }
-		$mysqli = new mysqli('193.107.29.49','xzindor_db1','lol123','xzindor_db1');
-		$sql2 = "select idbruker from bruker where epost= '$epost';";
-		$resultat = $mysqli->query($sql2);
-               
-                $valg = mysqli_fetch_row($resultat);
-		//mysql sjekk her
-		$idbruker = $valg[0];
-               
-		$resultat = "";
-		$sql = "Insert into ordre(ordredato,idbruker,betaltdato) 
-		Values(
-		'$ordredato',
-		'$idbruker',
-                '$ordredato')";
-		$resultat = $mysqli->query($sql);
+		  
+			$mysqli = parent::connect();
+			$sql2 = "select idbruker from bruker where epost= '$epost';";
+			$resultat = $mysqli->query($sql2);
+        	  if(!$resultat);
+					{
+						echo "Error".$db->error;
+					}     
+     	   $valg = mysqli_fetch_row($resultat);
+			$idbruker = $valg[0];
+            $mysqli->query($sql2);
+			$resultat = "";// feil her !!! !!!!!!!§!!// feil her !!! !!!!!!!§!!// feil her !!! !!!!!!!§!!// feil her !!! !!!!!!!§!!// feil her !!! !!!!!!!§!!// feil her !!! !!!!!!!§!!// feil her !!! !!!!!!!§!!
+			$sql = "Insert into ordre(idordre,ordredato,idbruker)  
+			Values(
+			'$ordreid',
+				'$ordredato',
+      	          '$idbruker')";
+			$resultat = $mysqli->query($sql);
+			$ordreid = $mysqli->insert_id;
 			if(!$resultat);
-			//mysql sjekk
+					{
+						echo "Error222".$db->error;
+					}
+			return $ordreid;
+			$mysqli->query();
+		  }
+		  else {
+			  return null;
+		  }
 	}
 }
